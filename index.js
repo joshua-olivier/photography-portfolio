2edc45681c1e7b08535c49bf98752c28ee5404b9
joshua-olivier <108597396+joshua-olivier@users.noreply.github.com>
2022-10-15 20:00:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//Function in JavaScript made for the loader animation
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
